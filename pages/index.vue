66c7431f6cf06e83991af41f22ea6ba8da48b4cd
Emanuel Carrero <89891608+emanuelmcp@users.noreply.github.com>
2023-11-23 00:29:41 +0100

    fix: :ambulance: fixed hydration errors (#21)
    fixed hydration errors
    
    Co-authored-by: Adariel <emanuelmcp@protonmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,16 @@ useSeoMeta({
 </script>
 
 <template>
-  <UPageHero
+  <ULandingHero orientation="horizontal" :ui="{ wrapper: 'md:py-20' }"
     description="Descubre cómo la destreza en fullstack puede revolucionar tu negocio. Soy un desarrollador fullstack con un historial comprobado de construcción de aplicaciones web que son tan robustas como intuitivas. Especializado en tecnologías modernas de frontend y backend, ofrezco soluciones que no solo cumplen, sino que superan las expectativas de rendimiento y usabilidad. Este portafolio demuestra mi compromiso con la excelencia en desarrollo web, la arquitectura de software eficiente y la creación de experiencias de usuario inolvidables. Con cada proyecto, tu visión se convierte en una aplicación web funcional y estéticamente agradable que impulsa el crecimiento."
-    align="left" :links="[
+    title="Experto en Desarrollo Fullstack" align="left" :links="[
       { label: 'Sobre mi', icon: '', color: 'green', size: 'lg', to: '/about' },
       { label: 'Contáctame', color: 'gray', size: 'lg', trailingIcon: 'i-heroicons-arrow-right-20-solid', to: '/contact' }
     ]">
-    <template #title>
-      <h2>Experto en Desarrollo Fullstack</h2>
-    </template>
-    <NuxtImg height="592" width="498" alt="Emanuel Carrero" format="webp" src="emanuel-carrero.webp" />
-  </UPageHero>
+    <div class="hidden lg:block px-4">
+      <NuxtImg height="592" width="498" alt="Emanuel Carrero" format="webp" src="emanuel-carrero.webp" />
+    </div>
+  </ULandingHero>
   <UPage>
     <TechSkills />
   </UPage>
