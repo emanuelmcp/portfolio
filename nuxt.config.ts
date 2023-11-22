8d0f6de0d76f8f9a658ce9d95177715eafb9e1aa
Adariel <emanuelmcp@protonmail.com>
2023-11-22 21:25:04 +0100

    feat: :zap: added ssg for main route
    Added ssg for main route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,8 +9,8 @@ export default defineNuxtConfig({
   colorMode: {
     preference: "dark",
   },
-  routeRules: {
-    "/": { prerender: true },
+  generate: {
+    routes: ["/"],
   },
   vite: {
     vue: {
